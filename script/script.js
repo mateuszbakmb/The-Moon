718b766b9d116bebe43dd6a0d548448807de1fc5
mateuszbak.mb <mateuszbak.mb@gmail.com>
2019-01-24 17:41:44 +0100

    new images for a loading screen and additional slide

--- script/script.js
+++ script/script.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
+const loadingScreen = document.querySelector('.loadingScreen')
+setTimeout(()=>{
+  loadingScreen.classList.toggle('opacity')
+  setTimeout( ()=>{loadingScreen.classList.toggle('hidden')},1000)
+},3500)
+
 const start = document.querySelector('#startButton')
 start.addEventListener('click', ()=>{
   document.querySelector('.moonOrbit').classList.toggle('hidden')
   start.classList.toggle('hidden')
-  console.log("still in progress")
 })
 
 for (let i=1; i<200; i++){
@@ -20,4 +25,23 @@ moonStart.addEventListener('click', ()=>{
   document.querySelector('.moonLanding').classList.toggle('hidden')
 })
 
+const theFlag = document.querySelector('#theFlag')
+theFlag.addEventListener('click', ()=>{
+  document.querySelector('#module').classList.toggle('hidden')
+  document.querySelector('#astro').classList.toggle('hidden')
+  theFlag.classList.toggle('hidden')
+  document.querySelector('#americanFlag').classList.toggle('hidden')
+  document.querySelector('#goBack').classList.toggle('hidden')
+})
+
+const goBack = document.querySelector('#goBack')
+goBack.addEventListener('click', ()=>{
+  document.querySelector('.moonLanding').classList.toggle('hidden')
+  document.querySelector('#module').classList.toggle('hidden')
+  document.querySelector('#astro').classList.toggle('hidden')
+  theFlag.classList.toggle('hidden')
+  document.querySelector('#americanFlag').classList.toggle('hidden')
+  goBack.classList.toggle('hidden')
+  start.classList.toggle('hidden')
+})
 })
